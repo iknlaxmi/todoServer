c198a8b5d723af2beb982788be1abde4ced7a827
admin <admin@Michaels-MacBook-Air.local>
2023-11-02 18:51:11 +0530

    CRUD implemented

--- todoServer.js
+++ todoServer.js
@@ -1,13 +1,53 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const todoList=['Running','Gardening','Bake cake'];
+let todoList = [
+  { id: "1", task: "Running" },
+  { id: "2", task: "Baking" },
+  { id: "3", task: "Reading" },
+];
+let countId = 3;
+const PORT = 3000;
 
-const PORT=3000;
+//to parse json requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//get all tasks
+app.get("/todos", (req, res) => {
+  res.send(todoList);
+});
 
-//get request
-app.get('/todos',(req,res)=>{
-    res.send(todoList);
-})
+//get single task
+app.get("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  //get single task from array
+  const requested_task = todoList.find((item) => item.id === id);
+  res.send(requested_task);
+});
+//Create single task
+app.post("/todos", (req, res) => {
+  console.log(req.body);
+  todoList.push(req.body);
+  res.send("Ok");
+});
+//Update single task
+app.put("/todos/:id", (req, res) => {
+  const modified_id = req.params.id;
 
-app.listen(PORT,console.log(`Server is listening on ${PORT}`));
+  const new_task = req.body.task;
+
+  const mod_task = todoList.find((item) => item.id === modified_id);
+
+  if (mod_task) {
+    mod_task.task = new_task;
+    console.log(todoList);
+  }
+  res.send("ok");
+});
+//delete single task
+app.delete("/todos/:id", (req, res) => {
+  const delete_id = req.params.id;
+  todoList = todoList.filter((item) => item.id !== delete_id);
+  res.send("Ok");
+});
+app.listen(PORT, console.log(`Server is listening on ${PORT}`));
